7dbf646ed7a2fc0e6fe243e235321ed40c993897
stephenavocado <stephenwalter@chicagobooth.edu>
2021-11-27 18:03:18 +0000

    started controller tests

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,6 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Project, type: :model do
+
+  it "can have many notes" do
+    project = FactoryBot.create(:project, :with_notes)
+    expect(project.notes.length).to eq 5
+  end
+
+  it "is late when the due date is past today" do
+    project = FactoryBot.create(:project, :due_yesterday)
+    expect(project).to be_late
+  end
+
+  it "is on time when the due date today" do
+    project = FactoryBot.create(:project, :due_today)
+    expect(project).to_not be_late
+  end
+
+  it "is on time when the due date is in the future" do
+    project = FactoryBot.create(:project, :due_tomorrow)
+    expect(project).to_not be_late
+  end
+
   it "does not allow duplicate project names per user" do
     user = User.create(
       first_name: "Joe",
